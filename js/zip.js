325d9d0af578929510df6417e01bdc505cbd26e5
psico-mulciber <158625834+Mulc1b3R@users.noreply.github.com>
2024-06-14 18:30:27 +0100

    Add files via upload

---
+++ js/zip.js
@@ -0,0 +1,9 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.zip = {}));
+})(this, (function (exports) { 'use strict';
+
+	const { Array, Object, String, Number, BigInt, Math, Date, Map, Set, Response, URL, Error, Uint8Array, Uint16Array, Uint32Array, DataView, Blob, Promise, TextEncoder, TextDecoder, document, crypto, btoa, TransformStream, ReadableStream, WritableStream, CompressionStream, DecompressionStream, navigator, Worker } = typeof globalThis !== 'undefined' ? globalThis : this || self;
+
+	var _documentCurrentScript = typeof document !== 'undefined' ? document.currentScript : null;
